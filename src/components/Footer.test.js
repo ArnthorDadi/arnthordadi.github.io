6abaf438027a87db1f79111894052e24c36ca697
ArnthorDadi <you@example.com>
2021-05-25 14:14:41 +0000

    Init commit

--- src/components/Footer.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Footer from './Footer';
-
-test('renders learn react link', () => {
-  render(<Footer />);
-  const linkElement = screen.getByText(/Arnþór Daði Jónasson/i);
-  expect(linkElement).toBeInTheDocument();
-});
