e97503a31cdbce0de6356c8d4256439fe8a88d5f
Arnþór Daði Jónasson <arnthordj@gmail.com>
2021-05-28 21:32:00 +0000

    Merging

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer";
-import Resume from "./components/Resume/Resume";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//import Preloader from "../src/components/Pre";
+//import Navbar from "./components/Navbar";
+//import Home from "./components/Home/Home";
+//import About from "./components/About/About";
+//import Projects from "./components/Projects/Projects";
+//import Footer from "./components/Footer";
+//import Resume from "./components/Resume/Resume";
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
 
-import ScrollToTop from "./components/ScrollToTop";
+//import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -21,6 +21,13 @@ function App() {
     }, 1200);
   }, []);
 
+  return (
+    <>
+      <h1>This is the home page, {load}</h1>
+      <p>This is the first paragraph</p>
+    </>
+  )
+  /*
   return (
     <Router>
       <Preloader load={load} />
@@ -37,6 +44,7 @@ function App() {
       </div>
     </Router>
   );
+  */
 }
 
 export default App;
